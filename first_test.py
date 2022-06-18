cec99119b85bd3be06d5ae27856e075f88468583
Битехтина Ольга <102225143+OlgaBit@users.noreply.github.com>
2022-06-18 13:34:24 +0300

    Create first_test.py

---
+++ first_test.py
@@ -0,0 +1,25 @@
+import pytest
+from app.calculator import Calculator
+
+
+class TestCalc:
+    def setup(self):
+        self.calc = Calculator
+
+    def test_multiply_calculate_correctly(self):
+        assert self.calc.multiply(self, 2, 2) == 4
+
+    def test_multiply_calculation_failed(self):
+        assert self.calc.multiply(self, 2, 2) == 5
+
+    def test_multiply_calculator_correct(self):
+        assert self.calc.divission(self, 8, 2) == 4
+
+    def test_multiply_calculator_failed(self):
+        assert self.calc.divission(self, 8, 2) == 6
+
+    def test_multiply_calculator_correct(self):
+        assert self.calc.subtraction(self, 15, 5) == 10
+
+    def test_multiply_calculator_failed(self):
+        assert self.calc.adding(self, 5, 5) == 11
